8cef0f87825eda0549c45cf1e952ffa9aecf1c3e
Refanzzzz <refandasuryasaputra@gmail.com>
2023-10-07 12:15:39 +0700

    create logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
+import java.math.BigInteger
+import java.util.Scanner
+import kotlin.math.pow
+
 fun main(args: Array<String>) {
 
+    val input = Scanner(System.`in`)
+
+    var inputNumber:Long? = null
+
+    while ((inputNumber ?: 0) < 1) {
+        print("Input maximum integer: ")
+        inputNumber = input.nextLong()
+    }
+
+    for (i in 1.. (inputNumber ?: 0)) {
+        println("Current Number is : $i and the cube is ${(i.toDouble().pow(3.0)).toLong()}")
+    }
 }
